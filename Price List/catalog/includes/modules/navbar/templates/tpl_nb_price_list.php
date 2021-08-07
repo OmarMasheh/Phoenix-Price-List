2b08f86f92503c72a3f3166e72998a12911a6fe1
Omar Masheh <36455369+OmarMasheh@users.noreply.github.com>
2021-08-07 13:50:04 +0300

    Update tpl_nb_price_list.php

--- Price List/catalog/includes/modules/navbar/templates/tpl_nb_price_list.php
+++ Price List/catalog/includes/modules/navbar/templates/tpl_nb_price_list.php
@@ -1,4 +1,13 @@
 <?php
+/*
+  $Id$
 
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
 
 echo '<a class="nav-link" href="' . tep_href_link('list_price.php') . '">' . MODULE_NAVBAR_PRICE_LIST_PUBLIC_TEXT . '</a>';
