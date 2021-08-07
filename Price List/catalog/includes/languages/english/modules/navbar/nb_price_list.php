16c520ba869c666c52f63018d61d1a807b8d0061
Omar Masheh <36455369+OmarMasheh@users.noreply.github.com>
2021-08-07 13:51:15 +0300

    Update nb_price_list.php

--- Price List/catalog/includes/languages/english/modules/navbar/nb_price_list.php
+++ Price List/catalog/includes/languages/english/modules/navbar/nb_price_list.php
@@ -1,7 +1,15 @@
 <?php
+/*
+  $Id$
 
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
 
   const MODULE_NAVBAR_PRICE_LIST_TITLE = 'Price list';
   const MODULE_NAVBAR_PRICE_LIST_DESCRIPTION = 'Show link to Price list in Navbar.';
-
-  define('MODULE_NAVBAR_PRICE_LIST_PUBLIC_TEXT', '<i title="Prices list"></i><span>Price list</span>');
+  const MODULE_NAVBAR_PRICE_LIST_PUBLIC_TEXT=  '<i title="Prices list"></i><span>Price list</span>';
